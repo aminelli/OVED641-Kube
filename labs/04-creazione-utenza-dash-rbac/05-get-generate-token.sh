66d2ca9d4b50c8abd1350813964b46a2d8815308
aminelli <antonio.minelli@etlforma.com>
2025-07-15 16:13:07 +0200

    upd

--- labs/04-creazione-utenza-dash-rbac/05-get-generate-token.sh
+++ labs/04-creazione-utenza-dash-rbac/05-get-generate-token.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #microk8s kubectl secret kubernetes-dashboard -n default 
-microk8s kubectl secret admin-user -n default -o jsonpath="{.data.token}" | base64 -d
+microk8s kubectl get secret admin-user -n default -o jsonpath="{.data.token}" | base64 -d
 
 
--- labs/comandi utili.md
+++ labs/comandi utili.md
@@ -3,3 +3,7 @@
 
 microk8s status --wait-ready
 
+# ottieni tutti i service account del cluster
+microk8s kubectl get sa --all-namespaces
+
+```
