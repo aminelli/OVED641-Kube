ed2976488d7bc3e0c8180a4da363fd9fa55241d7
aminelli <antonio.minelli@etlforma.com>
2025-07-15 16:42:58 +0200

    upd

--- labs/04-creazione-utenza-dash-rbac/06-all.sh
+++ labs/04-creazione-utenza-dash-rbac/06-all.sh
@@ -2,6 +2,6 @@
 
 microk8s kubectl apply -f 01-service-account.yaml
 microk8s kubectl apply -f 02-cluster-role-binding.yaml
-call 03-generate-token.sh
+bash 03-generate-token.sh
 microk8s kubectl apply -f 04-secret.yaml
-call 05-get-generate-token.sh
+bash 05-get-generate-token.sh
