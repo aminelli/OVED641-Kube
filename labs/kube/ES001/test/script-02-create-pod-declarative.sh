92a24befdea42d18db6545fa38ccc3019d1584b1
aminelli <antonio.minelli@etlforma.com>
2025-07-21 15:35:40 +0200

    upd

--- labs/kube/ES001/test/script-02-create-pod-declarative.sh
+++ labs/kube/ES001/test/script-02-create-pod-declarative.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 microk8s kubectl apply -f kube-pod.yaml
+
+
+kubectl get pods
+kubectl get pods --watch
+kubectl get pods hazelcast
+kubectl describe pods hazelcast
+kubectl describe pods hazelcast | grep Image
+kubectl logs hazelcast
+kubectl exec -it hazelcast -- /bin/sh
+
+
