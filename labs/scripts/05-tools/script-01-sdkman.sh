bbe32d7eb4611fd303d3ab2e5f6625ff628129b0
aminelli <antonio.minelli@etlforma.com>
2025-07-21 11:55:31 +0200

    upd

--- labs/scripts/05-tools/script-01-sdkman.sh
+++ labs/scripts/05-tools/script-01-sdkman.sh
@@ -5,3 +5,7 @@ curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk version
 
+sdk install java 17.0.12-oracle
+sdk install java 21.0.8-oracle
+
+sdk install maven 3.9.11
