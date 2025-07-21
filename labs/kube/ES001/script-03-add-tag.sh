c4002c14894d6a49e465b2b8fba6a628c8ed5af4
aminelli <antonio.minelli@etlforma.com>
2025-07-21 14:29:35 +0200

    upd

---
+++ labs/kube/ES001/script-03-add-tag.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker tag spring-app-demo:1.1.0 aminelli/spring-app-demo:1.1.0
+
+docker images
---
+++ labs/kube/ES001/script-04-pub.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker login --username=aminelli
+docker push aminelli/spring-app-demo:1.1.0
+
+
