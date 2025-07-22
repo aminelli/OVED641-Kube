75ace50f4a844532ee019e01421276289a08c3df
aminelli <antonio.minelli@etlforma.com>
2025-07-22 14:51:06 +0200

    upd

--- labs/kube/ES002-Net/TEST002/script-01-create-service.sh
+++ labs/kube/ES002-Net/TEST002/script-01-create-service.sh
@@ -1,3 +1,4 @@
 #!/Nin/bash
 
 kubectl run nginx --image nginx --expose=true --port=80
+
--- labs/kube/ES002-Net/TEST002/script-02-get-status.sh
+++ labs/kube/ES002-Net/TEST002/script-02-get-status.sh
@@ -5,3 +5,4 @@ read -p "Press [Enter] to continue after checking the pod and service status..."
 kubectl get po nginx --show-labels
 read -p "Press [Enter] to continue after checking the pod labels..."
 kubectl describe svc nginx
+
